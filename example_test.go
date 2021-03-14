6d11080986a76dbdc35da673d5dc1a3a90cc2bbb
Roman Kredentser <shareed2k@gmail.com>
2021-03-14 22:08:20 +0200

    add ttl by default

---
+++ example_test.go
@@ -0,0 +1,32 @@
+package badgerengine_test
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/dgraph-io/badger/v3"
+	"github.com/genjidb/genji"
+	"github.com/shareed2k/badgerengine"
+)
+
+func Example() {
+	dir, err := ioutil.TempDir("", "badger")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	ng, err := badgerengine.NewEngine(badger.DefaultOptions(filepath.Join(dir, "badger")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := genji.New(context.Background(), ng)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/shareed2k/badgerengine
+
+go 1.15
+
+require (
+	github.com/dgraph-io/badger/v3 v3.2011.1
+	github.com/genjidb/genji v0.10.2-0.20210314061331-aa68a5c314cc
+	github.com/stretchr/testify v1.7.0
+)
